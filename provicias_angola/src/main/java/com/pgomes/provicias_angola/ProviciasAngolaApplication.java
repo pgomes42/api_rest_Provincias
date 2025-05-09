414dda3e472e1ebc903cd52fdbc45f3643408360
pgomes42 <pgomes@student.42.fr>
2025-05-09 11:02:52 +0100

    primeiro

---
+++ provicias_angola/src/main/java/com/pgomes/provicias_angola/ProviciasAngolaApplication.java
@@ -0,0 +1,13 @@
+package com.pgomes.provicias_angola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProviciasAngolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProviciasAngolaApplication.class, args);
+	}
+
+}
---
+++ provicias_angola/src/test/java/com/pgomes/provicias_angola/ProviciasAngolaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pgomes.provicias_angola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProviciasAngolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
